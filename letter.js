26a8c112e36b3e2e86d3a47236bc3147bcd13196
Mogkc <carson.mogk@cox.net>
2019-03-22 10:26:34 -0700

    working class

---
+++ letter.js
@@ -0,0 +1,14 @@
+class Letter {
+    constructor(oneLetter) {
+        this.guessed = false;
+        this.name = oneLetter;
+    }
+    get() { 
+        return (this.guessed) ? this.name : "_";
+    }
+    guess(char) {
+        if(char.toUpperCase() === this.name.toUpperCase()) {
+            this.guessed = true;
+        }
+    }
+}
